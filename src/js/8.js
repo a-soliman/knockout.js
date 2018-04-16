30a38afa5f75e08a4d549b28aa6d2ca91bca2f93
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-04-16 11:09:52 -0700

    for each binding

---
+++ src/js/8.js
@@ -0,0 +1,17 @@
+let empoloyees = ko.observableArray([
+		{
+			id: 'e100',
+			name: 'Tom Jackson'
+		},
+		{
+			id: 'e101',
+			name: 'Jane Hopson'
+		},
+		{
+			id: 'e102',
+			name: 'Mike Rechardson'
+		},
+	])
+
+
+ko.applyBindings()
