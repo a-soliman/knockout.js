0a3e13da7a4a4d3ab47b4f89c6ac28ac4dfe1251
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-04-16 12:59:12 -0700

    more with value binding and check binding

--- src/js/9.js
+++ src/js/9.js
@@ -1,6 +1,15 @@
 function viewModel(){
 	self = this;
-	self.name = ko.observable('John')
+	self.name = ko.observable('John');
+	self.checkIt = ko.observable(false)
+	self.toggleBox = function(){
+		if (self.checkIt() == true){
+			self.checkIt(false)
+		}
+		else {
+			self.checkIt(true)
+		}
+	}
 
 }
 
